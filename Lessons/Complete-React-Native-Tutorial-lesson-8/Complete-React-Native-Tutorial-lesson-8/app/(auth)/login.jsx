9fd4e3865d492ce4593f8e41ec9b270013755cba
Kayombul <kayombul@gmail.com>
2025-09-25 22:56:47 +0200

    Lesson 8: Pressable components. Added pressable buttons to login and register

--- Lessons/Complete-React-Native-Tutorial-lesson-8/Complete-React-Native-Tutorial-lesson-8/app/(auth)/login.jsx
+++ Lessons/Complete-React-Native-Tutorial-lesson-8/Complete-React-Native-Tutorial-lesson-8/app/(auth)/login.jsx
@@ -1,26 +1,28 @@
-import { StyleSheet, Text } from "react-native";
-import { Link } from "expo-router";
-import { Colors } from "../../constants/Colors";
+import { StyleSheet, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
-import ThemedView from "../../components/ThemedView";
-import ThemedText from "../../components/ThemedText";
-import Spacer from "../../components/Spacer";
-import ThemedButton from "../../components/ThemedButton";
+import ThemedView from '../../components/ThemedView'
+import ThemedText from '../../components/ThemedText'
+import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
   const handleSubmit = async () => {
-    console.log("login form submitted");
-  };
+    console.log('login form submitted')
+  }
 
   return (
     <ThemedView style={styles.container}>
+      
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
 
       <ThemedButton onPress={handleSubmit}>
-        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+        <Text style={{ color: '#f2f2f2' }}>Login</Text>
       </ThemedButton>
 
       <Spacer height={100} />
@@ -29,22 +31,23 @@ const Login = () => {
           Register instead
         </ThemedText>
       </Link>
-    </ThemedView>
-  );
-};
 
-export default Login;
+    </ThemedView>
+  )
+}
+
+export default Login
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
     fontSize: 18,
-    marginBottom: 30,
+    marginBottom: 30
   },
   btn: {
     backgroundColor: Colors.primary,
@@ -52,6 +55,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   pressed: {
-    opacity: 0.8,
-  },
-});
+    opacity: 0.8
+  }
+})
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,23 +1,38 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 import { Link } from "expo-router";
-import React from "react";
+import { Colors } from "../../constants/Colors";
 
-// Themed components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import ThemedButton from "../../components/ThemedButton";
 
 const Register = () => {
+  const handleSubmit = async () => {
+    console.log("register form submitted");
+  };
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
-        Register for an Account
+        Register an Account
       </ThemedText>
 
+      {/* <Pressable
+        onPress={handleSubmit}
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <Text style={{color: "#f2f2f2"}}>Register</Text>
+      </Pressable> */}
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Register</Text>
+      </ThemedButton>
+
       <Spacer height={100} />
-      <Link href="/login">
-        <ThemedText style={{ textAlign: "center" }}>Login</ThemedText>
+      <Link href="/login" replace>
+        <ThemedText style={{ textAlign: "center" }}>Login instead</ThemedText>
       </Link>
     </ThemedView>
   );
@@ -29,10 +44,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });
